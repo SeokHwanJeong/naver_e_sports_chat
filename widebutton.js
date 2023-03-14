ed65528e9535dd4fe6a8c62197ff15636aceaa51
SeokHwanJeong <utopia0682@naver.com>
2023-03-14 19:51:55 +0900

    1

--- widebutton.js
+++ widebutton.js
@@ -1,4 +1,4 @@
-const elements = document.querySelector('.nng_btn_control.wide');
+const wideScreenElement = document.querySelector('.nng_btn_control.wide');
 
 // wideScreenElement.addEventListener("click", async () => {
 //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
