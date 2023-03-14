07bbb66ff912460e041208a8d79f33d2c2574bb1
SeokHwanJeong <utopia0682@naver.com>
2023-03-14 18:20:28 +0900

    test

--- widebutton.js
+++ widebutton.js
@@ -1,10 +1,12 @@
 const wideScreenElement = document.querySelector('.nng_btn_control.wide');
 
-wideScreenElement.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+// wideScreenElement.addEventListener("click", async () => {
+//     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageWatchChat,
-    });
-});
+//     chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     function: setPageWatchChat,
+//     });
+// });
+
+wideScreenElement.addEventListener("click", setPageWatchChat);
