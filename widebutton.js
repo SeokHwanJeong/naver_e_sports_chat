c38739bbd0967e96c5453c63a66c4722bc7aae9a
SeokHwanJeong <utopia0682@naver.com>
2023-03-14 22:11:30 +0900

    1

--- widebutton.js
+++ widebutton.js
@@ -1,4 +1,6 @@
-const wideScreenElement = document.querySelector('.nng_btn_control.wide');
+setTimeout(function() {
+    const wideScreenElement = document.querySelector('.nng_btn_control.wide');
+}, 2000);
 
 // wideScreenElement.addEventListener("click", async () => {
 //     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
